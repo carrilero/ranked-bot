68b7a9810e009a6d0f3b20aab6fd234965805388
carrilero <jcgrtorre13@gmail.com>
2025-05-19 20:51:53 +0200

    BOT BETA 1.0, cambiamos a 2

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD_ID = int(os.getenv("GUILD_ID", 0))  # opcional
-QUEUE_SIZE = 8  # Número de jugadores para iniciar una partida
+QUEUE_SIZE = 2  # Número de jugadores para iniciar una partida
 
 # Canales y roles (puedes adaptarlo a IDs fijos si lo prefieres)
 LOBBY_CHANNEL_NAME = "ranked-lobby"
